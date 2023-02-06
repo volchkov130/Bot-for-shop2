f4d761d20c6c23d1c41f5634b8ff319a398b955d
Kateryna <volka2003@tut.by>
2023-02-06 20:39:44 +0300

    Initial commit

---
+++ setting.py
@@ -0,0 +1,9 @@
+import telebot
+
+botToken = '6188763467:AAFR2QFX0tereD0PWdvFn0le18AoE8shrOQ'
+bot = telebot.TeleBot (botToken)
+#https://t.me/test_06022023_bot
+
+
+id_channel = '@test_canal'
+#https://t.me/+VJylJ1N-IRA3NTMy
